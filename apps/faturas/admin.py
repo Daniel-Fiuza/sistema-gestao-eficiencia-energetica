c4e2a48b7f63983750afaba84f1bd38f3a8bc036
Daniel_Fiuza <danielfiuza@outlook.com>
2023-02-22 04:28:58 -0300

    adicionado DadosFaturas no admin

--- apps/faturas/admin.py
+++ apps/faturas/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Faturas
+from .models import Faturas, DadosFaturas
 
 # Register your models here.
 admin.site.register(Faturas)
+admin.site.register(DadosFaturas)
